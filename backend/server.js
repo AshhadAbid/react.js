bee3a840d9ba3c4b302d37c6a26c5b78c08a2637
AshhadAbid <133577886+AshhadAbid@users.noreply.github.com>
2024-10-06 20:19:09 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,12 @@ app.use(cors({
 }));
 
 // Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: false,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('Error connecting to MongoDB:', err));
 
 // Session setup
 app.use(session({
